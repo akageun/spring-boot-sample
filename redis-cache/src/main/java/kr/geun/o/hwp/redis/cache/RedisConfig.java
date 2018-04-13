8c5b518aa67dce9443e9f31c5d634de2343653c2
“akageun” <akageun@gmail.com>
2018-04-14 00:37:46 +0900

    add redis configuraion

--- redis-cache/src/main/java/kr/geun/o/hwp/redis/cache/RedisConfig.java
+++ redis-cache/src/main/java/kr/geun/o/hwp/redis/cache/RedisConfig.java
@@ -1,8 +1,11 @@
 package kr.geun.o.hwp.redis.cache;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -15,42 +18,55 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
+	@Value("${spring.redis.host}")
+	private String redisHost;
 
-    @Value("${spring.redis.port}")
-    private int redisPort;
+	@Value("${spring.redis.port}")
+	private int redisPort;
 
-    @Value("${spring.redis.database}")
-    private int redisDatabase;
+	@Value("${spring.redis.database}")
+	private int redisDatabase;
 
-    /**
-     * Factory
-     *
-     * @return
-     */
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName(redisHost);
-        jedisConnectionFactory.setPort(redisPort);
-        jedisConnectionFactory.setDatabase(redisDatabase);
-        jedisConnectionFactory.setUsePool(true);
-        return jedisConnectionFactory;
-    }
+	/**
+	 * Factory
+	 *
+	 * @return
+	 */
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+		jedisConnectionFactory.setHostName(redisHost);
+		jedisConnectionFactory.setPort(redisPort);
+		jedisConnectionFactory.setDatabase(redisDatabase);
+		jedisConnectionFactory.setUsePool(true);
+		return jedisConnectionFactory;
+	}
 
-    /**
-     * redis Template
-     *
-     * @return
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+	/**
+	 * redis Template
+	 *
+	 * @return
+	 */
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
 
-        return redisTemplate;
-    }
+		return redisTemplate;
+	}
+
+	/**
+	 * Cache Manager
+	 *
+	 * @param redisTemplate
+	 * @return
+	 */
+	@Bean
+	public CacheManager cacheManager(RedisTemplate redisTemplate) {
+		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+		cacheManager.setDefaultExpiration(300); // Number of seconds before expiration. Defaults to unlimited (0)
+		return cacheManager;
+	}
 }
